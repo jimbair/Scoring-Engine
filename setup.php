5bd75c7e378f8665ca28f3c8cd9102c3696c9c59
Reed Wilkins <reedwilkins@gmail.com>
2011-02-14 14:01:57 -0600

    Added poller file location to db

--- setup.php
+++ setup.php
@@ -24,7 +24,7 @@
 
 	// Create new tables
 	try {
-		mysql_query("CREATE TABLE services ( name varchar(10) NOT NULL, teamid int(10) NOT NULL, attempts INT(10) DEFAULT 0, success INT(10) DEFAULT 0, lastcheck INT(1) DEFAULT 0, active INT(1) DEFAULT 0, useauth INT(1) DEFAULT 0, host VARCHAR(30) NOT NULL, user VARCHAR(25), pass VARCHAR(25) )");
+		mysql_query("CREATE TABLE services ( name varchar(10) NOT NULL, teamid int(10) NOT NULL, attempts INT(10) DEFAULT 0, success INT(10) DEFAULT 0, lastcheck INT(1) DEFAULT 0, active INT(1) DEFAULT 0, useauth INT(1) DEFAULT 0, host VARCHAR(30) NOT NULL, user VARCHAR(25), pass VARCHAR(25), poller VARCHAR(25) NOT NULL )");
 		mysql_query("CREATE TABLE teams ( id INT(10) NOT NULL AUTO_INCREMENT, name VARCHAR(25) NOT NULL, PRIMARY KEY(id) )");
 	} catch (Exception $e) {
 		print "Could not create tables: $e->getMessage()\n";
