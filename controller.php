c6b7265ce833272d115ad3827bf415032a0b1783
Reed Wilkins <reedwilkins@gmail.com>
2011-02-20 03:29:13 -0600

    Minor bugfixes

--- controller.php
+++ controller.php
@@ -2,9 +2,8 @@
 
 <?
 require('class/ccdc.class.php');
-$basedir = 'pollers/';
-print $_SERVER['DOCUMENT_ROOT'];
-
+$basedir = dirname($_SERVER['PHP_SELF']) . '/pollers/';
+print "\nPoller Path: $basedir \n";
 // Change max execution time to 1 minute
 set_time_limit(60);
 
